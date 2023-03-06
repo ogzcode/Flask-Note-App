95ced39a28544b33754abf4155d2b8dd3d8c9af5
ogzCode <oguzguc44@gmail.com>
2023-03-06 11:54:02 +0300

    Edit route added

--- app.py
+++ app.py
@@ -56,3 +56,25 @@ def delete(id):
     conn.close()
     
     return redirect(url_for("main"))
+
+@app.route('/<int:id>/edit', methods=("GET", "POST"))
+def edit(id):
+    note = get_note(id)
+
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+
+        if not title:
+            flash("Title is required!!")
+        elif not content:
+            flash("Conten is required!!")
+        else:
+            conn = get_db_connection()
+            conn.execute("UPDATE notes SET title = ?, content = ? WHERE id = ?", (title,content,id))
+            conn.commit()
+            conn.close()
+
+            return redirect(url_for("main"))
+        
+    return render_template("edit.html", note=note)
